f49d9765fc84cf2533ac0e5485ec411acde2a968
aesatchien <cjh123@yahoo.com>
2023-02-15 21:43:07 -0800

    added TOF and turret tests

--- tests/distance-test/robot.py
+++ tests/distance-test/robot.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 """
-
+2023 0215 CJH
+Test for getting rage measurements back from the PWF TimeofFlight sensor
+10.24.29.2:5812  - should be a web page that lets you config the TOF sensor
 """
 
 import wpilib
@@ -10,12 +12,27 @@ from playingwithfusion import TimeOfFlight
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self) -> None:
-        self.DistSensor = TimeOfFlight(13)
+        self.joystick = wpilib.Joystick(0)
+        self.DistSensor = TimeOfFlight(0)  # ships with an ID of 0
         self.DistSensor.setRangingMode(TimeOfFlight.RangingMode.kShort, 50)
+        self.counter = 0
+        # self.DistSensor.
 
     def robotPeriodic(self) -> None:
-        wpilib.SmartDashboard.putNumber("distance", self.DistSensor.getRange())  # should be mm to target
-        wpilib.SmartDashboard.putNumber("status", self.DistSensor.getStatus())  # should be mm to target
+
+        self.counter += 1
+
+        b1 = self.joystick.getRawButton(1)
+        b2 = self.joystick.getRawButton(2)
+        if b1:
+            self.DistSensor.identifySensor()
+        if b2:
+            msg = f'distance: {self.DistSensor.getRange():0.1f}'
+            print(msg, end='\r')
+
+        if self.counter % 10 == 0:
+            wpilib.SmartDashboard.putNumber("distance", self.DistSensor.getRange())  # should be mm to target
+            wpilib.SmartDashboard.putNumber("status", self.DistSensor.getStatus())  # should be mm to target
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
--- tests/turret-test/robot.py
+++ tests/turret-test/robot.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""  20230215 CJH
+Test robot designed to:
+ drive the turret from a sparkmax/neo combo
+ read from the default encoder
+ read from the absolute encoder
+"""
+
 import wpilib
 import rev
 
@@ -9,29 +16,31 @@ class MyRobot(wpilib.TimedRobot):
 
         self.joystick = wpilib.Joystick(0)
 
-        self.turret_controller = rev.CANSparkMax(6, rev.CANSparkMax.MotorType.kBrushless)
-        #self.absolute_encoder =  self.turret_controller.getAbsoluteEncoder(encoderType= rev.SparkMaxAbsoluteEncoder)
+        self.turret_controller = rev.CANSparkMax(9, rev.CANSparkMax.MotorType.kBrushless)
+        self.absolute_encoder = self.turret_controller.getAbsoluteEncoder(encoderType=rev.SparkMaxAbsoluteEncoder.Type.kDutyCycle)
         self.default_encoder = self.turret_controller.getEncoder()
-        self.alternate_encoder = self.turret_controller.getAlternateEncoder(1)
+
+        # not allowed to have an absolute encoder and an alternate encoder
+        #self.alternate_encoder = self.turret_controller.getAlternateEncoder(1)
 
     def teleopPeriodic(self) -> None:
-        b1 = self.joystick.getRawButton(0)
-        b2 = self.joystick.getRawButton(1)
+        b1 = self.joystick.getRawButton(1)
+        b2 = self.joystick.getRawButton(2)
 
         stick = self.joystick.getY()
         self.turret_controller.set(stick)
 
-
         if b1:
-            pass
+            msg = f'default encoder: {self.default_encoder.getPosition():0.1f}  absolute encoder: {self.absolute_encoder.getPosition():0.1f}'
+            print(msg, end='\r')
         if b2:
             pass
 
-
         wpilib.SmartDashboard.putNumber("output", stick)
         wpilib.SmartDashboard.putNumber("encoder", self.default_encoder.getPosition())
-        #wpilib.SmartDashboard.putNumber("absolute_encoder", self.absolute_encoder.getPosition())
-        wpilib.SmartDashboard.putNumber("alternate_encoder", self.alternate_encoder.getPosition())
+        wpilib.SmartDashboard.putNumber("absolute_encoder", self.absolute_encoder.getPosition())
+        #wpilib.SmartDashboard.putNumber("alternate_encoder", self.alternate_encoder.getPosition())
+
 
 
 if __name__ == "__main__":
