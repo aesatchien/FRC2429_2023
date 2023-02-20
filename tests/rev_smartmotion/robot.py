74c45dd7b2ff99b857f8aa72f9c6770396ddd2b4
aesatchien <cjh123@yahoo.com>
2023-02-19 21:12:20 -0800

    tidying up controller configs

--- tests/rev_smartmotion/robot.py
+++ tests/rev_smartmotion/robot.py
@@ -4,7 +4,8 @@
 # NOTE SmartMotion only uses ONE PIDF slot - the one for velocity.  You aren't actually changing position control PIDs.
 # Also not the trickery about conversion factors in the SparkMax and how they affect decel and PIDFs
 # (they all scale with the factor, and if you change position, also change velocity)
-# Good starting values for NEO motor with no load - probably need to bump kFF first.  With no load I'm not seeing much kp influence.
+# Good starting values for NEO motor with no load  below.
+# probably need to bump kFF first once we have a load.  With no load I'm not seeing much kp influence.
 # Conversion factor = 1 :  kp 5e-5, ki 1e-6, kd 0, kiz 1e-5, kFF 1.6e-4
 # Conversion factor = 0.1 :  kp 5e-5, ki 1e-6, kd 0, kiz 1e-5, kFF 1.9e-3
 
