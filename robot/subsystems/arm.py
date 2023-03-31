7c0ed6a5ec8b9ec8b597b6910c9e14d68a276920
aesatchien <cjh123@yaoo.com>
2023-03-31 07:05:31 -0700

    adding a quiet arm feature

--- robot/subsystems/arm.py
+++ robot/subsystems/arm.py
@@ -20,6 +20,7 @@ class Arm(SubsystemBase):
     def __init__(self):
         super().__init__()
         self.counter = 0
+        self.motion_log_counter = 0
 
         self.max_extension = 580  # need to see what is max legal amount
         self.min_extension = 2  # mm for now
@@ -39,7 +40,7 @@ class Arm(SubsystemBase):
         self.arm_controller.enableSoftLimit(rev.CANSparkMax.SoftLimitDirection.kReverse, True)
         self.arm_controller.setSoftLimit(rev.CANSparkMax.SoftLimitDirection.kForward, self.max_extension)
         self.arm_controller.setSoftLimit(rev.CANSparkMax.SoftLimitDirection.kReverse, self.min_extension)
-        # self.arm_controller.setSmartCurrentLimit()
+        self.arm_controller.setSmartCurrentLimit(20)  # is 20 amps enough?
         self.pid_controller.setSmartMotionAllowedClosedLoopError(1)
 
         configure_sparkmax(sparkmax=self.arm_controller, pid_controller=self.pid_controller, slot=0, can_id=constants.k_arm_motor_port,
@@ -56,6 +57,8 @@ class Arm(SubsystemBase):
         SmartDashboard.putNumber('arm_extension', self.extension)
         SmartDashboard.putNumber('arm_setpoint', self.setpoint)
         self.is_moving = False
+        self.motion_log = [False] * 5
+        self.movement_commanded = False
 
     def get_extension(self):  # getter for the relevant elevator parameter
         if wpilib.RobotBase.isReal():
@@ -71,6 +74,7 @@ class Arm(SubsystemBase):
             # just use the position PID
             self.pid_controller.setReference(distance, rev.CANSparkMax.ControlType.kPosition)
 
+        self.movement_commanded = True  # keep track of if we have told the arm to move
         self.setpoint = distance
         SmartDashboard.putNumber('arm_setpoint', self.setpoint)
 
@@ -83,9 +87,28 @@ class Arm(SubsystemBase):
         if wpilib.RobotBase.isSimulation():
             self.extension = distance
 
+    def quiet_arm(self):  # the motor keeps oscillating after a smart motion
+        # setting it to current locations seems to quiet it
+        current_extension = self.get_extension()
+        self.set_arm_extension(current_extension)
+
     def periodic(self) -> None:
         self.counter += 1
         if self.counter % 25 == 0:
             self.extension = self.get_extension()
             SmartDashboard.putNumber('arm_extension', self.extension)
             self.is_moving = abs(self.sparkmax_encoder.getVelocity()) > 100  #
+
+            # TODO - see if arm motor is oscillating trying to maintain a setpoint and quiet it.
+            self.motion_log_counter += 1
+            self.motion_log[self.motion_log_counter % len(self.motion_log)] = self.is_moving
+            if any(self.motion_log):  # we've been moving at some point in the past second
+                pass
+            else:  # we've been sitting still for a while
+                pass
+                # if self.movement_commanded:
+                #     self.movement_commanded = False
+                #     self.quiet_arm()
+
+        # seems to quiet down if you set the setpoint to the current location
+
